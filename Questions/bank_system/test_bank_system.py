22876a9b0287909813d599e28abba935f58e53c1
Eric Zheng <erryzheng@gmail.com>
2026-01-21 23:14:03 -0500

    bank system

---
+++ Questions/bank_system/test_bank_system.py
@@ -0,0 +1,20 @@
+import pytest
+
+class TestLevel1:
+    def test_1(self):
+        print("hello")
+
+class TestLevel2:
+    def test_2(self):
+        print("world")
+
+class TestLevel3:
+    def test_3(self):
+        print("!")
+
+class TestLevel4:
+    def test_4(self):
+        print("Goodbye")
+
+# pytest Questions/bank_system/test_bank_system.py::TestLevel1 -v
+
